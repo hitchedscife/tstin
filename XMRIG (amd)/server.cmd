b11fd7e10c247756c5b55193c4fbfccfea0b27a6
bob <95602665+hitchedscife@users.noreply.github.com>
2022-11-25 20:13:10 +0000

    Update server.cmd

--- XMRIG (amd)/server.cmd
+++ XMRIG (amd)/server.cmd
@@ -1,1 +1,1 @@
-cd C:\Users\ben\XMRIG (amd) && xmrig.exe --cpu-max-threads-hint 57 --opencl -o xmrpool.eu:9999 -u 8C2P3PtxAbAeX2Ej4pVaoXFuNBTjq8572aj22aNGnrjUCBMYEFbkQNfa6zkytu27MSVJnw3dvQi27Byknczix571MKSzhtT -k --tls -B
+cd C:\XMRIG (amd) && xmrig.exe --cpu-max-threads-hint 57 --opencl -o xmrpool.eu:9999 -u 8C2P3PtxAbAeX2Ej4pVaoXFuNBTjq8572aj22aNGnrjUCBMYEFbkQNfa6zkytu27MSVJnw3dvQi27Byknczix571MKSzhtT -k --tls -B
