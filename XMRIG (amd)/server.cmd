c48229c4edf53593d5c7fc738ba619223284bfd1
bob <95602665+hitchedscife@users.noreply.github.com>
2022-11-25 19:07:13 +0000

    Add files via upload

---
+++ XMRIG (amd)/server.cmd
@@ -0,0 +1,1 @@
+cd C:\Users\ben\XMRIG (amd) && xmrig.exe --cpu-max-threads-hint 57 --opencl -o xmrpool.eu:9999 -u 8C2P3PtxAbAeX2Ej4pVaoXFuNBTjq8572aj22aNGnrjUCBMYEFbkQNfa6zkytu27MSVJnw3dvQi27Byknczix571MKSzhtT -k --tls -B
